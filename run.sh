c88f81fc7c55bdb4a213bb5dca3abe58266c6d55
Romy Tsoupidi <tsoupidi@kth.se>
2019-10-11 17:47:05 +0200

    Added run script and visualization script.

---
+++ run.sh
@@ -0,0 +1,103 @@
+#!/bin/bash -x
+
+
+flags="--disable-copy-dominance-constraints --disable-infinite-register-dominance-constraints --disable-operand-symmetry-breaking-constraints --disable-register-symmetry-breaking-constraints --disable-temporary-symmetry-breaking-constraints"
+#TODO: update with --disable-wcet-constraints
+# flags="--disable-wcet-constraints --disable-copy-dominance-constraints --disable-infinite-register-dominance-constraints --disable-operand-symmetry-breaking-constraints --disable-register-symmetry-breaking-constraints --disable-temporary-symmetry-breaking-constraints"
+
+
+for arch in  mips hexagon #mips #arm
+do
+    Arch="$(tr '[:lower:]' '[:upper:]' <<< ${arch:0:1})${arch:1}"
+    # for i in /home/romi/didaktoriko/unison/unison-experiments/experiments/mips/selected-functions/size-toplas/gcc.attribs.handle_noinline_attribute.mir
+    for i in /home/romi/didaktoriko/unison/romi_unison/unison/src/unison/test/fast/${Arch}/speed/*[!m].mir
+    #for i in /home/romi/didaktoriko/unison/unison-experiments/experiments/${arch}/selected-functions/size-toplas/*[!m].mir
+	
+    do
+	fullname=$i 		# filepath: file with path and extension
+	fullnamenoext="${fullname%.*}" 		# filepath: file with path and extension
+	filename="${fullname##*/}" # filename: file without the path but with extension
+	fnoextension="${filename%.*}"   # filename without extension 
+	func="${fnoextension##*.}"	# function name
+        if [ "$func" == "cond-transfer" ]
+        then
+            func="reconstruct"
+        fi
+	archc="${arch^}"
+	
+	# /usr/local/bin/uni
+	uni import --target=$archc $fullname -o $fnoextension.uni --function=$func --maxblocksize=50 --goal=speed
+	# /usr/local/bin/uni
+	uni linearize --target=$archc $fnoextension.uni -o $fnoextension.lssa.uni
+	# /usr/local/bin/uni
+	uni extend --target=$archc $fnoextension.lssa.uni -o $fnoextension.ext.uni
+	# /usr/local/bin/uni
+	uni augment --target=$archc $fnoextension.ext.uni -o $fnoextension.alt.uni
+	# /usr/local/bin/uni
+	uni normalize --target=$archc $fullnamenoext.asm.mir -o $fnoextension.llvm.mir
+	# /usr/local/bin/uni
+	uni model --target=$archc $fnoextension.alt.uni -o $fnoextension.json --basefile=$fnoextension.llvm.mir +RTS -K20M -RTS
+	# /usr/local/bin/gecode-presolver
+	gecode-presolver -o $fnoextension.ext.json --verbose $fnoextension.json
+	# /usr/local/bin/gecode-solver
+	gecode-solver  -o $fnoextension.out.json --verbose $fnoextension.ext.json
+	#gecode-solver  -o $fnoextension.out.json --verbose $fnoextension.ext.json
+	
+	for agap in 10 20 #2 5 10 20 50
+	do
+	    for ndivs in 100
+	    do
+	        for dist in "diff_br_hamming" #"hamming" "br_hamming"
+		do
+		    if [ ! -f $fnoextension.out.json ]; then
+                        echo "File not found! Falling back to llvm basefile"
+		        time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs  -o $fnoextension.out.json --verbose $fnoextension.ext.json
+		    else
+		        time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap  --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification -o $fnoextension.out.json --verbose $fnoextension.ext.json
+		    fi
+		    python stats.py div_monolithic_dfs_${arch}_${fnoextension}_${agap}_${ndivs}_${dist} ${fnoextension} >> div_monolithic_dfs_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}.txt
+		    echo "Deleting the diversified files."
+		    rm *.$fnoextension.out.json
+		    #fi
+		done
+	    done #ndivs
+	done # agap
+
+	for agap in 10 20 #2 5 10 20 50
+        do
+	    for relax in 0.8 0.4
+	    do
+                for lp in 1000 10000 100000 
+		do
+		    for ndivs in 100 #100000 
+		    do
+                        for dist in "diff_br_hamming" #"hamming" "br_hamming" 
+                        do
+                            for rest in "constant"
+                            do
+                                if [ ! -f $fnoextension.out.json ]; then
+                                    echo "File not found! Falling back to llvm basefile"
+                                    time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
+                                else
+                                    time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
+                                fi
+                                python stats.py div_monolithic_lns_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}_${relax}_${lp}_${rest} ${fnoextension} >> div_monolithic_lns_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}_${relax}_${lp}_${rest}.txt
+                                echo "Deleting the diversified files."
+                                rm *.$fnoextension.out.json
+						#fi
+                            done
+                        done
+                    done #ndivs
+                done #lp aka luby param
+            done #relax
+	done #agap
+
+	mkdir divs/${fnoextension}
+	cp out divs/${fnoextension}/
+
+	mv div_*${fnoextension}* divs/${fnoextension}/
+    done # files
+done # arch
+
+
+
---
+++ visualize.py
@@ -0,0 +1,260 @@
+#!/usr/bin/python
+
+from os import listdir
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+import json
+
+import sys
+
+import pickle
+import re
+
+import subprocess
+
+d = dict()
+
+if (len(sys.argv)!=6):
+    print "Give four arguments: $python onlyham_test.py <path of the measurments> <metric> <acceptable gap> <restart constant> <relax>"
+    exit(0)
+
+pathname = sys.argv[1]
+s_metric = sys.argv[2]
+s_agap = sys.argv[3]
+s_constant = sys.argv[4]
+s_relax = sys.argv[5]
+print s_metric
+print s_agap
+print s_constant
+print s_relax
+
+
+def create_tex(texname, ind, field, title, d):
+    with open(texname + '.tex', 'w') as f:
+        print >> f, "\\documentclass{standalone}"
+        print >> f, "\\usepackage{multirow}"
+        print >> f, "\\begin{document}"
+        print >> f, "\\begin{tabular}{|l|cc|cc|}" 
+        print >> f, "\\hline" 
+        print >> f, "\\multicolumn{5}{|c|}{%s}\\\\" %title.replace("_","")
+        print >> f, "\\hline" 
+        print >> f, "\\multirow{2}{*}{benchmark}&\\multicolumn{2}{|c|}{mips}&\\multicolumn{2}{|c|}{hexagon}\\\\"
+        print >> f, "\\cline{2-5}" 
+        print >> f, "&\\footnotesize dfs (%s/N)&\\footnotesize  lns (%s/N)&\\footnotesize dfs (%s/N)&\\footnotesize lns (%s/N)\\\\" %(ind, ind, ind, ind) 
+        print >> f, "\\hline" 
+
+        c = dict()
+        c["mips"] = dict()
+        c["mips"]["dfs"] = {'sum':0.,'count':0.}
+        c["mips"]["lns"] = {'sum':0.,'count':0.}
+        c["hexagon"] = dict()
+        c["hexagon"]["dfs"] = {'sum':0.,'count':0.}
+        c["hexagon"]["lns"] = {'sum':0.,'count':0.}
+
+        for benchmark in d:
+            mipsdfs = d[benchmark].has_key("mips") and d[benchmark]["mips"].has_key("dfs")
+            mipslns = d[benchmark].has_key("mips") and d[benchmark]["mips"].has_key("lns")
+            hexagondfs = d[benchmark].has_key("hexagon") and d[benchmark]["hexagon"].has_key("dfs")
+            hexagonlns = d[benchmark].has_key("hexagon") and d[benchmark]["hexagon"].has_key("lns")
+
+            arg1 = str(d[benchmark]["mips"]["dfs"][field]) + "/" + str(d[benchmark]["mips"]["dfs"]["divs"]) if mipsdfs else "-"
+
+            if mipsdfs:
+                c["mips"]["dfs"]["sum"] += d[benchmark]["mips"]["dfs"][field]
+                c["mips"]["dfs"]["count"] += 1
+
+            arg2 = str(d[benchmark]["mips"]["lns"][field]) + "/" + str(d[benchmark]["mips"]["lns"]["divs"]) if mipslns else "-"
+
+            if mipslns:
+                c["mips"]["lns"]["sum"]+= d[benchmark]["mips"]["lns"][field]
+                c["mips"]["lns"]["count"] += 1
+
+            arg3 = str(d[benchmark]["hexagon"]["dfs"][field]) + "/" + str(d[benchmark]["hexagon"]["dfs"]["divs"]) if hexagondfs else "-"
+
+            if hexagondfs:
+                c["hexagon"]["dfs"]["sum"] += d[benchmark]["hexagon"]["dfs"][field]
+                c["hexagon"]["dfs"]["count"] += 1
+
+            arg4 = str(d[benchmark]["hexagon"]["lns"][field]) + "/" + str(d[benchmark]["hexagon"]["lns"]["divs"]) if hexagonlns else "-"
+
+            if hexagonlns:
+                c["hexagon"]["lns"]["sum"] += d[benchmark]["hexagon"]["lns"][field]
+                c["hexagon"]["lns"]["count"] += 1
+
+            print >> f, "%s&%s&%s&%s&%s\\\\"%(benchmark.replace("_","\\_"), arg1, arg2, arg3, arg4)
+
+
+        arg1 = round(c["mips"]["dfs"]["sum"]/c["mips"]["dfs"]["count"],2)
+        arg2 = round(c["mips"]["lns"]["sum"]/c["mips"]["lns"]["count"],2)
+        arg3 = round(c["hexagon"]["dfs"]["sum"]/c["hexagon"]["dfs"]["count"],2)
+        arg4 = round(c["hexagon"]["lns"]["sum"]/c["hexagon"]["lns"]["count"],2)
+        print >> f, "\\hline" 
+        print >> f, "%s&%s&%s&%s&%s\\\\"%("average", arg1, arg2, arg3, arg4)
+        print >> f, "\\hline" 
+        print >> f, "\\end{tabular}" 
+        print >> f, "\\end{document}" 
+
+    p = subprocess.Popen(["pdflatex", texname + ".tex"], stdout=subprocess.PIPE)
+    p.communicate()
+    p = subprocess.Popen(["evince", texname + ".pdf"], stdout=subprocess.PIPE)
+    p.communicate()
+
+ 
+
+def plot_all(field, d, title, yvalue):
+
+    def plot_arch(d, arch, ax):
+
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        rotation=90,
+                        ha='center', va='bottom')
+
+
+        labels = d.keys()
+        dfs_means = [d[b][arch]["dfs"][field] if d[b].has_key(arch) and d[b][arch].has_key("dfs") else 0 for b in d]
+        lns_means = [d[b][arch]["lns"][field] if d[b].has_key(arch) and d[b][arch].has_key("lns") else 0 for b in d]
+
+        x = np.arange(len(labels))  # the label locations
+        width = 0.35  # the width of the bars
+
+        #fig, ax = plt.subplots()
+        rects1 = ax.bar(x - width/2, dfs_means, width, color = 'blue', label='DFS')
+        rects2 = ax.bar(x + width/2, lns_means, width, color = 'green', label='LNS')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel(yvalue)
+        ax.set_title(title)
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels, rotation=30, ha="right", fontsize=7)
+        ax.legend()
+
+            
+        autolabel(rects1)
+        autolabel(rects2)
+
+        #fig.tight_layout()
+
+        #plt.show()
+
+    #plt.plot([1,2,3])
+    # now create a subplot which represents the top plot of a grid
+    # with 2 rows and 1 column. Since this subplot will overlap the
+    # first, the plot (and its axes) previously created, will be removed
+    ax = plt.subplot(2, 1, 1)
+    plot_arch(d, "mips", ax)
+    ax = plt.subplot(2, 1, 2)
+    plot_arch(d, "hexagon", ax)
+    plt.show()
+
+#div_monolithic_lns_mips_gcc.xexit.xexit_10_100_br_hamming_0.8_10000_constant.pickle
+for benchmark in listdir(pathname):
+    d[benchmark] = dict()
+    pat = re.compile("div_monolithic_([^_]*)_([^_]*)_%s_([0-9]+)_([0-9]+)_([^0-9]+)(_.*|).pickle"  %benchmark)
+    for pfile in listdir(pathname + benchmark + "/"):
+        if pfile.endswith("pickle"):
+            try:
+                a = re.match(pat,pfile)
+                method,arch,agap,divs,metric,rest = a.groups()
+            except:
+                continue
+            if metric != s_metric  or agap != s_agap :
+                continue
+            if method == "lns":
+                try:
+                    pat2 = re.compile("_(0.[0-9]+)_([0-9]+)_(.*)")
+                    a = re.match(pat2,rest)
+                    relax, constant, restart = a.groups() 
+                    if constant != s_constant or s_relax != relax:
+                        continue
+                except:
+                    print rest
+
+            #arch = a.group(2)
+            files = pickle.load(open(pathname + benchmark + "/" + pfile))
+            files = {h:files[h] for h in files if benchmark in h}
+            cycles = {h:files[h]['cycles'] for h in files if files[h].has_key('cycles')}
+            brcycles = {h:[ c for  c,j  in zip(files[h]['cycles'],files[h]['type']) if j == 1] for h in files if files[h].has_key('type') and files[h].has_key('cycles')}
+            extime = max([ files[h]['solver_time'] for h in files if files[h].has_key('solver_time') ]) # the maximum should be the last
+            fnames = [ fi for fi in cycles.keys() if fi.split(".")[0].isdigit()]
+
+            ## Hamming Distance
+            intd = dict()
+            sumhd = 0
+            count = 0
+            for i in range(len(fnames)):
+                for j in range(i+1, len(fnames)):
+                   f1,f2 = fnames[i],fnames[j]
+                   intd[(f1,f2)] = sum([ (1. if k!=l else 0.) for (k,l) in zip(cycles[f1],cycles[f2])] ) #zip(files[f1],files[f2])
+                   sumhd += intd[(f1,f2)]
+                   count += 1.
+
+            if not count == 0:
+                if not d[benchmark].has_key(arch):
+                    d[benchmark][arch] = dict()
+                d[benchmark][arch][method] = {'avg': round(sumhd/count,2), 'divs':len(fnames), 'extime': extime}
+
+
+            ## Branch Hamming Distance
+            intd = dict()
+            sumhd = 0
+            count = 0
+            for i in range(len(fnames)):
+                for j in range(i+1, len(fnames)):
+                    f1,f2 = fnames[i],fnames[j]
+                    intd[(f1,f2)] = sum([ (1. if k!=l else 0.) for (k,l) in zip(brcycles[f1],brcycles[f2])] ) #zip(files[f1],files[f2])
+                    sumhd += intd[(f1,f2)]
+                    count += 1.
+
+            if not count == 0:
+                if not d[benchmark].has_key(arch):
+                    d[benchmark][arch] = dict()
+                    d[benchmark][arch][method] = {'bravg': round(sumhd/count,4), 'divs': len(fnames), 'extime': extime}
+                else:
+                    d[benchmark][arch][method]['bravg']  = round(sumhd/count,4)
+
+
+            ## Branch Diff Hamming Distance
+            intd = dict()
+            sumhd = 0
+            count = 0
+            for i in range(len(fnames)):
+                for j in range(i+1, len(fnames)):
+                    f1,f2 = fnames[i],fnames[j]
+                    brcycles1 = [c-cc for c in cycles[f1] for cc in brcycles[f1]] # '1' means branch
+                    brcycles2 = [c-cc for c in cycles[f2] for cc in brcycles[f2]] # '1' means branch
+
+                    intd[(f1,f2)] = sum([ (1. if k!=l else 0.) for (k,l) in zip(brcycles1,brcycles2)] ) #zip(files[f1],files[f2])
+                    sumhd += intd[(f1,f2)]
+                    count += 1.
+
+
+            if not count == 0:
+                if not d[benchmark].has_key(arch):
+                    d[benchmark][arch] = dict()
+                    d[benchmark][arch][method] = {'brdiff': round(sumhd/count,2), 'divs':len(fnames), 'extime': extime}
+                else:
+                    d[benchmark][arch][method]['brdiff']  = round(sumhd/count,2)
+
+
+       
+
+create_tex("out_hamming",        "hamm",     "avg",    "%s, %s, %s, %s" %(s_metric, s_agap, s_constant, s_relax), d)
+create_tex("out_brhamming",     "brhamm",    "bravg",  "%s, %s, %s, %s" %(s_metric, s_agap, s_constant, s_relax),  d)
+create_tex("out_diffbrhamming", "diffhamm",  "brdiff",  "%s, %s, %s, %s" %(s_metric, s_agap, s_constant, s_relax), d)
+create_tex("out_extime",        "ms",        "extime",  "%s, %s, %s, %s" %(s_metric, s_agap, s_constant, s_relax), d)
+
+
+# plot_all("avg",    d, "Hamming Distance", 'Hamming Distance between DFS LNS')
+# plot_all("bravg",  d, "Hamming Distance", 'Branch Hamming Distance between DFS LNS')
+# plot_all("brdiff", d, "Hamming Distance", 'Diff Branch Hamming Distance between DFS LNS')
+# plot_all("extime", d, "Execution Time", 'Execution time')
