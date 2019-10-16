9c697282ca8ca6ca31ffc7acb89f6f50d32add70
Romy Tsoupidi <tsoupidi@kth.se>
2019-10-16 15:56:14 +0200

    Added checks and option to generate the folder.

--- run.sh
+++ run.sh
@@ -5,11 +5,25 @@ flags="--disable-copy-dominance-constraints --disable-infinite-register-dominanc
 #TODO: update with --disable-wcet-constraints
 # flags="--disable-wcet-constraints --disable-copy-dominance-constraints --disable-infinite-register-dominance-constraints --disable-operand-symmetry-breaking-constraints --disable-register-symmetry-breaking-constraints --disable-temporary-symmetry-breaking-constraints"
 
+#missing_files="/home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/mesa.api.glIndexd.mir /home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/sphinx3.glist.glist_tail.mir /home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/sphinx3.profile.ptmr_init.mir"
 
-for arch in  mips hexagon #mips #arm
+#test_files="/home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Mips/speed/gobmk.board.get_last_player.mir"
+
+DIVS_PATH=divs
+if [ $# -ge 1 ]
+then
+    DIVS_PATH=$1
+fi
+
+if [ ! -d $DIVS_PATH ]
+then
+    mkdir $DIVS_PATH
+fi
+
+ 
+for arch in mips hexagon #mips #arm
 do
     Arch="$(tr '[:lower:]' '[:upper:]' <<< ${arch:0:1})${arch:1}"
-    # for i in /home/romi/didaktoriko/unison/unison-experiments/experiments/mips/selected-functions/size-toplas/gcc.attribs.handle_noinline_attribute.mir
     for i in /home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/${Arch}/speed/*[!m].mir
     #for i in /home/romi/didaktoriko/unison/unison-experiments/experiments/${arch}/selected-functions/size-toplas/*[!m].mir
 	
@@ -47,13 +61,13 @@ do
 	do
 	    for ndivs in 100
 	    do
-	        for dist in "diff_br_hamming" #"hamming" "br_hamming"
+	        for dist in "diff_br_hamming" "hamming" "br_hamming"
 		do
 		    if [ ! -f $fnoextension.out.json ]; then
                         echo "File not found! Falling back to llvm basefile"
-		        time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs  -o $fnoextension.out.json --verbose $fnoextension.ext.json
+		        time timeout 5m gecode-diversify ${flags} --acceptable-gap $agap --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs  -o $fnoextension.out.json --verbose $fnoextension.ext.json
 		    else
-		        time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap  --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification -o $fnoextension.out.json --verbose $fnoextension.ext.json
+		        time timeout 5m gecode-diversify  ${flags} --acceptable-gap $agap  --div-method monolithic_dfs --distance ${dist} --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification -o $fnoextension.out.json --verbose $fnoextension.ext.json
 		    fi
 		    python stats.py div_monolithic_dfs_${arch}_${fnoextension}_${agap}_${ndivs}_${dist} ${fnoextension} >> div_monolithic_dfs_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}.txt
 		    echo "Deleting the diversified files."
@@ -65,21 +79,21 @@ do
 
 	for agap in 10 20 #2 5 10 20 50
         do
-	    for relax in 0.8 0.4
+	    for relax in 0.4 0.8
 	    do
                 for lp in 1000 10000 100000 
 		do
 		    for ndivs in 100 #100000 
 		    do
-                        for dist in "diff_br_hamming" #"hamming" "br_hamming" 
+                        for dist in "diff_br_hamming" "hamming" "br_hamming" 
                         do
                             for rest in "constant"
                             do
                                 if [ ! -f $fnoextension.out.json ]; then
                                     echo "File not found! Falling back to llvm basefile"
-                                    time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
+                                    time timeout 5m gecode-diversify  ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
                                 else
-                                    time timeout 5m gecode-diversify --timeout 600000 ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
+                                    time timeout 5m gecode-diversify  ${flags} --acceptable-gap $agap --relax $relax --seed 12 --distance ${dist} --restart $rest --restart-base $lp --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification --div-method monolithic_lns -o $fnoextension.out.json --verbose $fnoextension.ext.json
                                 fi
                                 python stats.py div_monolithic_lns_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}_${relax}_${lp}_${rest} ${fnoextension} >> div_monolithic_lns_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}_${relax}_${lp}_${rest}.txt
                                 echo "Deleting the diversified files."
@@ -92,10 +106,10 @@ do
             done #relax
 	done #agap
 
-	mkdir divs/${fnoextension}
-	cp out divs/${fnoextension}/
+	mkdir ${DIVS_PATH}/${fnoextension}
+	cp out ${DIVS_PATH}/${fnoextension}/
 
-	mv div_*${fnoextension}* divs/${fnoextension}/
+	mv div_*${fnoextension}* ${DIVS_PATH}/${fnoextension}/
     done # files
 done # arch
 
