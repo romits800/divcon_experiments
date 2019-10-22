8d9ab84a5e8b21f09337e317737ae602067f1371
Romy Tsoupidi <romi@s2656.ict.kth.se>
2019-10-22 16:46:49 +0200

    Removed code that generates histograms.

--- stats.py
+++ stats.py
@@ -27,52 +27,5 @@ if (len(sys.argv) > 1) and len(files)>0:
     import pickle
     pickle.dump(files, open(sys.argv[1] + ".pickle", "w"))
 
-for f in files:
-    try:
-        files[f] = files[f]['cycles']
-    except:
-        print "Error: no cycles"
-        print files[f]
-        continue
-
-print len(files)
-if (len(files) == 0):
-    exit(0)
-
-
-fnames = files.keys()
-d = dict()
-
-sumhd = 0
-count = 0
-for i in range(len(fnames)-1):
-    for j in range(i+1, i+1+len(fnames[i+1:])):
-       f1,f2 = fnames[i],fnames[j]
-       d[(f1,f2)] = sum([ (1 if k!=l else 0) for (k,l) in zip(files[f1],files[f2])] ) #zip(files[f1],files[f2])
-       sumhd += d[(f1,f2)]
-       count += 1
-
-dist = dict()
-# for files in d:
-#     if dist.has_key(d[files]):
-#         dist[d[files]] += 1
-#     else:
-#         dist[d[files]] = 1
-# print dist
-
-x = d.values()
-num_bins = max(x) - min(x)
-n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='blue', alpha=0.5)
-plt.xlabel("Hamming Distance")
-plt.ylabel("Frequency")
-
-if (len(sys.argv) > 1):
-    plt.title(sys.argv[1])
-    plt.savefig(sys.argv[1] + ".jpg")
-#    dist[files] = sum([ (1 if i!=j else 0) for (i,j) in d[files]] )
-
-print count
-print "Hdist:", (1.0*sumhd)/count
-
 
 
