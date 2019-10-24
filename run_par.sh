f28b99ba4a42a31551c923c7982549c7ea046200
Romy Tsoupidi <tsoupidi@kth.se>
2019-10-24 08:48:55 +0200

    Updated run_par to run for specified cpus.

--- run_par.sh
+++ run_par.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash -x
 
-for i in {1..5}
+for i in {0..4}
 do
-    bash -x ./run.sh divs_${i} divs_dir_${i} &> out_${i}
+    echo $i
+    hex="0x1"
+    cpu1=$((hex << i))
+    cpu2=$((cpu1 << 5))
+    m2=$((cpu1 | cpu2))
+    taskset `printf '0x%x\n' $m2` nohup ./run.sh divs_${i} divs_dir_${i} &> out_${i} &
 done
 
