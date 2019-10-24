a82bc8eb3b693a181c4f79393db7242323a1519a
Romy Tsoupidi <tsoupidi@kth.se>
2019-10-24 16:06:15 +0200

    Added run for all metrics.

--- run_par.sh
+++ run_par.sh
@@ -10,3 +10,17 @@ do
     taskset `printf '0x%x\n' $m2` nohup ./run.sh divs_${i} divs_dir_${i} &> out_${i} &
 done
 
+
+if [ $1 == "branch" ]
+then
+    for i in {0..4}
+    do
+        echo $i
+        hex="0x1"
+        cpu1=$((hex << i))
+        cpu2=$((cpu1 << 5))
+        m2=$((cpu1 | cpu2))
+        taskset `printf '0x%x\n' $m2` nohup ./run_br.sh divs_${i} divs_dir_${i} &> out_br_${i} &
+    done
+fi
+
