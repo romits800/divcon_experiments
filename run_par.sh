83f9cca34ffa1426e81311af3ccc031e4a95eb55
Romy Tsoupidi <tsoupidi@kth.se>
2019-11-03 16:26:36 +0100

    Add run_lev.sh for levenshtein distance.

--- run_par.sh
+++ run_par.sh
@@ -12,6 +12,17 @@ then
         m2=$((cpu1 | cpu2))
         taskset `printf '0x%x\n' $m2` nohup ./run_br.sh divs_${i} divs_dir_${i} &> out_br_${i} &
     done
+elif [ "$1" == "levenshtein" ]
+then
+    for i in {0..4}
+    do
+        echo $i
+        hex="0x1"
+        cpu1=$((hex << i))
+        cpu2=$((cpu1 << 5))
+        m2=$((cpu1 | cpu2))
+        taskset `printf '0x%x\n' $m2` nohup ./run_lev.sh divs_${i} divs_dir_${i} &> out_lev_${i} &
+    done
 else
     for i in {0..4}
     do
