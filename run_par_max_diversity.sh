b0e6553b6370b314ca868759b0e61ff689e63500
Romy Tsoupidi <tsoupidi@kth.se>
2019-11-24 13:20:50 +0100

    Split into 1 execution due to throttling.

--- run_par_max_diversity.sh
+++ run_par_max_diversity.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/bash -x
 
-for i in {0..2}
+for i in {0..0}
 do
-    echo $i
-    hex="0x1"
-    cpu=$((hex << i))
-    taskset `printf '0x%x\n' $cpu` nohup ./run_max_diversity_brham.sh divs_${i} divs_dir_${i} divs_br_${i} &> out_br_${i} &
+    taskset "0x3" nohup ./run_max_diversity_brham.sh divs_${i} divs_dir_${i} divs_br_${i} &> out_br_${i} &
 done
-for i in {0..2}
+for i in {0..0}
 do
-    echo $i
-    hex="0x8"
-    cpu=$((hex << i))
-    taskset `printf '0x%x\n' $cpu` nohup ./run_max_diversity_lev.sh divs_${i} divs_dir_${i} divs_lev_${i} &> out_lev_${i} &
+    taskset "0xc" nohup ./run_max_diversity_lev.sh divs_${i} divs_dir_${i} divs_lev_${i} &> out_lev_${i} &
 done
-for i in {0..2}
+for i in {0..0}
 do
-    echo $i
-    hex="0x40"
-    cpu=$((hex << i))
-    taskset `printf '0x%x\n' $cpu` nohup ./run_max_diversity_ham.sh divs_${i} divs_dir_${i} divs_ham_${i} &> out_ham_${i} &
+    taskset "0x30" nohup ./run_max_diversity_ham.sh divs_${i} divs_dir_${i} divs_ham_${i} &> out_ham_${i} &
 done
-for i in {0..2}
+for i in {0..0}
 do
-    echo $i
-    hex="0x200"
-    cpu=$((hex << i))
-    taskset `printf '0x%x\n' $cpu` nohup ./run_max_diversity_dham.sh divs_${i} divs_dir_${i} divs_diff_${i} &> out_diff_${i} &
+    taskset "0xc0" nohup ./run_max_diversity_dham.sh divs_${i} divs_dir_${i} divs_diff_${i} &> out_diff_${i} &
 done
 
