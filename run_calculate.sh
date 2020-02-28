f7210be3f8b34e6f4e7abf9331f68b6aca94f6ce
Romy Tsoupidi <tsoupidi@kth.se>
2020-02-28 10:02:23 +0100

    Change to calculate_max_div_2_nodata.

--- run_calculate.sh
+++ run_calculate.sh
@@ -2,6 +2,6 @@
 
 for i in 0 1 2 3 4
 do 
-    echo divs_${i}; 
-    python calculate.py -p divs_${i}/
+    echo divs_*_${i}/divs_${i}; 
+    taskset "0x800" python calculate_max_div_2_nodata.py -p divs_*_${i}/divs_${i}/
 done
