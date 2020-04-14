adca9e2303478ab3df7842742a517b95b1d35e0d
Rodothea-Myrsini Tsoupidi <tsoupidi@kth.se>
2020-04-14 17:16:07 +0200

    Update results after correcting generate_results.py script.

--- scripts/generate_results.py
+++ scripts/generate_results.py
@@ -3,25 +3,31 @@
 
 import cPickle as pickle
 import visualize_interactive as vi
+import os
 
-d_max_div = pickle.load(open("max_div_divs.pickle"))
+res_folder = "../results/"
+pickles = "pickles"
+maxdiv = "maxdiv_lns_rs"
+rest = "rest"
+
+d_max_div = pickle.load(open(os.path.join(res_folder, pickles, "max_div_divs.pickle")))
 
 for b in vi.benchmarks:
-	vi.plot_maxdiv_lns_new(d_max_div, b, "hamming", "avg", 10, 0.8, "1", dist=False, path="results/maxdiv_lns_rs/")
-	vi.plot_maxdiv_lns_new(d_max_div, b, "hamming", "avg", 10, 0.8, "1", dist=True, path="results/maxdiv_lns_rs/")
+	vi.plot_maxdiv_lns_new(d_max_div, b, "hamming", "avg", 10, 0.8, "1", dist=False, path=os.path.join(res_folder, maxdiv))
+	vi.plot_maxdiv_lns_new(d_max_div, b, "hamming", "avg", 10, 0.8, "1", dist=True, path=os.path.join(res_folder, maxdiv))
 
 # LNS evaluation
-vi.tex_max_lns_rs(d_max_div, "hamming", "avg", 10, 200, "1", ["0.7"], texname='results/maxdiv_lns_rs/max_lns_rs_table', show=False)
+vi.tex_max_lns_rs(d_max_div, "hamming", "avg", 10, 200, "1", ["0.7"], texname=os.path.join(res_folder, maxdiv, "max_lns_rs_table"), show=False)
 
-d_rest = pickle.load(open("rest_divs.pickle"))
+d_rest = pickle.load(open(os.path.join(res_folder, pickles, "rest_divs.pickle")))
 
-vi.tex_agap(d_rest, "hamming", "avg", [0, 5, 10,20], 200, "1", "0.7", texname='results/rest/lns_gaps', show=False)
+vi.tex_agap(d_rest, "hamming", "avg", [0, 5, 10,20], 200, "1", "0.7", texname=os.path.join(res_folder, maxdiv, "lns_gaps"), show=False)
 
 # Distance evaluation
-vi.tex_distances(d_rest, "avg", "10", 200, "1", "0.7", ["hamming", "br_hamming", "levenshtein"], texname="results/rest/lns_distances", show=False)
+vi.tex_distances(d_rest, "avg", "10", 200, "1", "0.7", ["hamming", "br_hamming", "levenshtein"], texname=os.path.join(res_folder, rest, "lns_distances"), show=False)
 
 # Generate figures for appendix - Selection of relax rate
 colors = ['rosybrown', "firebrick", "darkred","chocolate", "darkorange", "khaki", "yellowgreen", "limegreen", 
 	  "darkgreen", "steelblue", "darkblue", "slateblue", "darkorchid","purple", "crimson"]
-vi.plot_rs_vs_lns(d_rest, "hamming", "avg", 10, colors, 200, "1", dist=False, path="results/rest/")
-vi.plot_rs_vs_lns(d_rest, "hamming", "avg", 10, colors, 200, "1", path="results/rest/")
+vi.plot_rs_vs_lns(d_rest, "hamming", "avg", 10, colors, 200, "1", dist=False, path=os.path.join(res_folder, rest))
+vi.plot_rs_vs_lns(d_rest, "hamming", "avg", 10, colors, 200, "1", path=os.path.join(res_folder, rest))
