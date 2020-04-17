3ec2bcdd1df3e7b3c559f17dcff59a7d1c412b39
Romy Tsoupidi <tsoupidi@kth.se>
2020-04-17 11:00:33 +0200

    Change number of variants.

--- factorial/run_test.sh
+++ factorial/run_test.sh
@@ -25,7 +25,7 @@ gecode-solver  -o $input.out.json --verbose $input.ext.json
 
 flags="--disable-copy-dominance-constraints --disable-infinite-register-dominance-constraints --disable-operand-symmetry-breaking-constraints --disable-register-symmetry-breaking-constraints --disable-temporary-symmetry-breaking-constraints --disable-wcet-constraints"
 
-gecode-diversify $flags --acceptable-gap 45 --relax 0.8 --seed 12 --number-divs 100 --restart constant --restart-base 10000 --distance levenshtein --div-method monolithic_lns -o $input.out.json --use-optimal-for-diversification --solver-file $input.out.json  --branching random --verbose $input.ext.json
+gecode-diversify $flags --acceptable-gap 45 --relax 0.8 --seed 12 --number-divs 1000 --restart constant --restart-base 10000 --distance hamming --div-method monolithic_lns -o $input.out.json --use-optimal-for-diversification --solver-file $input.out.json  --branching clrandom --verbose $input.ext.json
 
 
 for i in *.${input}.out.json; 
