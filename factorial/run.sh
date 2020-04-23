f443500eed1b6f039896007ecf2c1fc793493c03
Romy Tsoupidi <tsoupidi@kth.se>
2020-04-23 19:10:04 +0200

    New scripts for factorial

---
+++ factorial/run.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash -x
+
+
+export PATH=${PATH}:${DIVCON_PATH}/src/solvers/gecode:${DIVCON_PATH}/src/solvers/multi_backend/minizinc/:${DIVCON_PATH}/src/solvers/multi_backend/:${MINIZINC_PATH}:${DIVCON_PATH}/src/solvers/multi_backend/common/ UNISON_DIR=${DIVCON_PATH}
+
+flags="--disable-copy-dominance-constraints --disable-infinite-register-dominance-constraints --disable-operand-symmetry-breaking-constraints --disable-register-symmetry-breaking-constraints --disable-temporary-symmetry-breaking-constraints --disable-wcet-constraints"
+
+#missing_files="/home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/mesa.api.glIndexd.mir /home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/sphinx3.glist.glist_tail.mir /home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Hexagon/speed/sphinx3.profile.ptmr_init.mir"
+
+#test_files="/home/romi/didaktoriko/unison/romi_unison/divCon/src/unison/test/fast/Mips/speed/gobmk.board.get_last_player.mir"
+# In kbytes: 10Gbytes
+ulimit -v 10485760
+
+
+cp ../scripts/stats.py .
+
+arch=mips
+Arch="$(tr '[:lower:]' '[:upper:]' <<< ${arch:0:1})${arch:1}"
+fullname=factorial.mir
+fullnamenoext="${fullname%.*}" 		# filepath: file with path and extension
+filename="${fullname##*/}" # filename: file without the path but with extension
+fnoextension="${filename%.*}"   # filename without extension 
+func="${fnoextension##*.}"	# function name
+archc="${arch^}"
+	
+	# /usr/local/bin/uni
+uni import --target=$archc $fullname -o $fnoextension.uni --function=$func --maxblocksize=50 --goal=size
+uni linearize --target=$archc $fnoextension.uni -o $fnoextension.lssa.uni
+uni extend --target=$archc $fnoextension.lssa.uni -o $fnoextension.ext.uni
+uni augment --target=$archc $fnoextension.ext.uni -o $fnoextension.alt.uni
+uni model --target=$archc $fnoextension.alt.uni -o $fnoextension.json 
+gecode-presolver -o $fnoextension.ext.json -dzn $fnoextension.dzn --verbose $fnoextension.json
+ ${DIVCON_PATH}/src/solvers/multi_backend/portfolio-solver -o $fnoextension.out.json --verbose $fnoextension.ext.json
+
+
+branch="clrandom"
+relax=0.7
+lp=500
+rest="constant"
+seed=111
+agap=10
+ndivs=100
+dist=hamming
+
+time timeout 10m gecode-diversify  ${flags} --acceptable-gap $agap --relax $relax --seed $seed --distance ${dist} --restart $rest --restart-scale $lp --number-divs $ndivs --solver-file $fnoextension.out.json --use-optimal-for-diversification --div-method monolithic_lns -o $fnoextension.out.json --enable-solver-solution-brancher --branching ${branch}  --verbose $fnoextension.ext.json
+python stats.py maxdiv_monolithic_lns_${arch}_${fnoextension}_${agap}_${ndivs}_${dist}_${branch}_${seed}_1_${relax}_${lp}_${rest} ${fnoextension} . .
+
+
+for i in *.out.json; 
+do 
+    #uni export --keepnops --target=Mips ${path}/${bench}.alt.uni -o $i.unison.mir --basefile=${path}/${bench}.llvm.mir --solfile=$i; 
+    uni export --keepnops --target=Mips ${fnoextension}.alt.uni -o $i.unison.mir --solfile=$i; 
+done
+
+
+for i in *.out.json.unison.mir
+do
+    inoext="${i%.*}"   # filename without extension 
+    inoext2="${inoext%.*}"   # filename without extension 
+    inoext3="${inoext2%.*}"   # filename without extension 
+    inoext4="${inoext3%.*}"   # filename without extension 
+llc $i  -filetype=obj -march=mipsel -mcpu=mips32 -disable-post-ra -disable-tail-duplicate -disable-branch-fold -disable-block-placement -start-after livedebugvars -o $inoext4.o
+done
+ 
--- factorial/stats.py
+++ factorial/stats.py
@@ -2,94 +2,32 @@
 
 from os import listdir
 
-import numpy as np
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
-
 import json
 
 import sys
 
+import pickle
+
 files = dict()
 
-if (len(sys.argv)<2):
-        print "Give as argument the benchmark name"
-        exit(0)
+if (len(sys.argv) != 5):
+    print "Give four arguments:"
+    print "python stats.py <picklename> <benchname> <divsdirname> <resultdirname>"
+    exit(0) 
 
-for f in listdir("."):
-    if f.endswith("." + sys.argv[1] + ".out.json"):
+pname,bench,ddir,rdir = tuple(sys.argv[1:])
+
+for f in listdir(ddir):
+    if f.endswith("." + bench + ".out.json"):
         try:
-            files[f] = json.loads(open(f).read())
+            files[f] = json.loads(open(ddir + "/" + f).read())
         except:
+            print "Exception in file:", f
             continue
 
-# if (len(sys.argv) > 1) and len(files)>0:
-    # import pickle
-    # pickle.dump(files, open(sys.argv[1] + ".pickle", "w"))
 
-for f in files:
-    try:
-        files[f] = files[f]['cycles']
-    except:
-        print "Error: no cycles"
-        print files[f]
-        continue
-
-print len(files)
-if (len(files) == 0):
-    exit(0)
-
-
-fnames = files.keys()
-d = dict()
-
-sumhd = 0
-count = 0
-for i in range(len(fnames)-1):
-    for j in range(i+1, i+1+len(fnames[i+1:])):
-       f1,f2 = fnames[i],fnames[j]
-       d[(f1,f2)] = sum([ (1 if k!=l else 0) for (k,l) in zip(files[f1],files[f2])] ) #zip(files[f1],files[f2])
-       sumhd += d[(f1,f2)]
-       count += 1
-
-# dist = dict()
-# for files in d:
-#     if dist.has_key(d[files]):
-#         dist[d[files]] += 1
-#     else:
-#         dist[d[files]] = 1
-# print dist
-freq = dict()
-for di in d:
-        if freq.has_key(d[di]):
-                freq[d[di]].append(di)
-        else:
-                freq[d[di]] = [di]
-
-for fi in freq:
-        print fi, len(freq[fi])
-
-mink = min(freq.keys())
-maxk = max(freq.keys())
-print mink
-print freq[mink][0]
-print maxk
-print freq[maxk][0]
-
-# x = d.values()
-# num_bins = max(x) - min(x)
-# n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='blue', alpha=0.5)
-# plt.xlabel("Hamming Distance")
-# plt.ylabel("Frequency")
-
-# if (len(sys.argv) > 1):
-#     plt.title(sys.argv[1])
-#     plt.show()
-#     # plt.savefig(sys.argv[1] + ".jpg")
-# #    dist[files] = sum([ (1 if i!=j else 0) for (i,j) in d[files]] )
-
-print count
-print "Hdist:", (1.0*sumhd)/count
+if (len(sys.argv) > 1) and len(files)>0:
+    pickle.dump(files, open(rdir + "/" + pname + ".pickle", "w"))
 
 
 
