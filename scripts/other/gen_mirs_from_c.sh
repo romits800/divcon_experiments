85077ea9a5ed3bebf5c07a83b302b57e252c4c9a
Romy Tsoupidi <tsoupidi@kth.se>
2020-11-20 15:32:58 +0100

    Add mipsel target in clang and comment the sodium compilation scripts.

--- scripts/other/gen_mirs_from_c.sh
+++ scripts/other/gen_mirs_from_c.sh
@@ -15,17 +15,18 @@ opt=-O2
 for cfile in $files
 #for llfile in ../unison/src/unison/test/fast/Mips/speed/*.ll
     do
-        clang-3.8 $CFLAGS -O2 -mllvm -debug-pass=Arguments -S -emit-llvm $cfile
+        clang-3.8 $CFLAGS -O2 -target mipsel -mllvm -debug-pass=Arguments -S -emit-llvm $cfile
         rm /tmp/unison-*
         fname1=${cfile##*/}
         fname=${fname1%.*}
+        function=fname
         if [ -f $ddir/$fname.asm.mir ]
         then
             continue
         fi
-        timeout 1m llc $fname.ll $opt -march=mips -mcpu=mips32 -unison -unison-no-clean
+        timeout 1m llc $fname.ll $opt -march=mips -mcpu=mips32 -unison -unison-no-clean -unison-only-functions="$function"
         cp /tmp/unison-*.asm.mir $ddir/$fname.asm.mir
         cp /tmp/unison-*[!mn].mir $ddir/$fname.mir
     done
-clang-3.8 -I ./crypto_libs/libsodium-stable/src/libsodium/include/ -I ./crypto_libs/libsodium-stable/src/libsodium/include/sodium/ -target mipsel  -O2  -S -emit-llvm ./crypto_libs/libsodium-stable/src/libsodium/crypto_core/salsa/ref/core_salsa_ref.c 
-llc core_salsa_ref.ll -O2 -march=mips -unison -unison-no-clean
+#clang-3.8 -I ./crypto_libs/libsodium-stable/src/libsodium/include/ -I ./crypto_libs/libsodium-stable/src/libsodium/include/sodium/ -target mipsel  -O2  -S -emit-llvm ./crypto_libs/libsodium-stable/src/libsodium/crypto_core/salsa/ref/core_salsa_ref.c 
+#llc core_salsa_ref.ll -O2 -march=mips -unison -unison-no-clean
