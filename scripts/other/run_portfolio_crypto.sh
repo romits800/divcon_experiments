4a82600d13914e94c8c53b8e33bf476c04a50ce4
Romy Tsoupidi <tsoupidi@kth.se>
2021-02-16 15:31:28 +0100

    Fix script for running crypto.

--- scripts/other/run_portfolio_crypto.sh
+++ scripts/other/run_portfolio_crypto.sh
@@ -2,45 +2,42 @@
 
 export PATH=${PATH}:${DIVCON_PATH}/src/solvers/gecode:${DIVCON_PATH}/src/solvers/multi_backend/minizinc/:${DIVCON_PATH}/src/solvers/multi_backend/:/home/romi/didaktoriko/misc/minizinc/MiniZincIDE-2.1.2-bundle-linux-x86_64/:${DIVCON_PATH}/src/solvers/multi_backend/common/ UNISON_DIR=${DIVCON_PATH} 
 
-ddir=$DIVCONEXP_PATH/mirfiles/Mips/crypto
-# Source directory
-crypto_names=$DIVCONEXP_PATH/data/crypto_filenames.txt
+ddir=$DIVCONEXP_PATH/mirfiles/Mips/crypto/bearSSL
+sfolder=$DIVCONEXP_PATH/solution_files/crypto
+#files=`cut -d' ' -f 1 $crypto_names`
+#funcs=`cut -d' ' -f 2 $crypto_names`
 
-files=`cut -d' ' -f 1 $crypto_names`
-#funcs="br_aes_big_encrypt br_aes_ct_bitslice_encrypt"
-funcs=`cut -d' ' -f 2 $crypto_names`
-
-afiles=($files)
-afuncs=($funcs)
-
-count=${#afiles[@]}
-for i in `seq 1 $count`
+for i in $ddir/*[!m].mir
 do
+    input=$i 		# filepath: file with path and extension
+    fullnamenoext="${input%.*}" # filepath: file with path and extension
+    filename="${input##*/}" # filename: file without the path but with extension
+    fnoextension="${filename%.*}"   # filename without extension 
+    func="${fnoextension##*.}"	# function name
+    sfile=$sfolder/$fnoextension
 
-    input=$ddir/${afiles[$i-1]}
-    func=${afuncs[$i-1]}
 
     # /usr/local/bin/uni
-    uni import --target=Mips $input.mir -o $input.uni --function=$func --maxblocksize=25 --goal=speed
+    uni import --target=Mips $input -o $sfile.uni --function=$func --maxblocksize=25 --goal=speed
     # /usr/local/bin/uni
-    uni linearize --target=Mips $input.uni -o $input.lssa.uni
+    uni linearize --target=Mips $sfile.uni -o $sfile.lssa.uni
     # /usr/local/bin/uni
-    uni extend --target=Mips $input.lssa.uni -o $input.ext.uni
+    uni extend --target=Mips $sfile.lssa.uni -o $sfile.ext.uni
     # /usr/local/bin/uni
-    uni augment --target=Mips $input.ext.uni -o $input.alt.uni
+    uni augment --target=Mips $sfile.ext.uni -o $sfile.alt.uni
     # /usr/local/bin/uni
-    uni normalize --target=Mips $input.asm.mir -o $input.llvm.mir
+    #uni normalize --target=Mips $sfile.asm.mir -o $sfile.llvm.mir
     # /usr/local/bin/uni
     #uni model --target=Mips $input.alt.uni -o $input.json --basefile=$input.llvm.mir +RTS -K20M -RTS
-    uni model --target=Mips $input.alt.uni -o $input.json  #+RTS -K20M -RTS
+    uni model --target=Mips $sfile.alt.uni -o $sfile.json  #+RTS -K20M -RTS
     # /usr/local/bin/gecode-presolver
-    gecode-presolver -t 1000000 -o $input.ext.json -dzn $input.dzn --verbose $input.json
+    gecode-presolver -t 1000000 -o $sfile.ext.json -dzn $sfile.dzn --verbose $sfile.json
     # /usr/local/bin/gecode-solver
     #gecode-solver  -o $input.out.json --verbose $input.ext.json
 
     #echo "/home/romi/didaktoriko/unison/romi_unison/unison/src/solvers/multi_backend/portfolio-solver -o $input.out.json --verbose $input.ext.json"
     #--global-budget 100
-    ${DIVCON_PATH}/src/solvers/multi_backend/portfolio-solver -o $input.out.json --verbose $input.ext.json
+    ${DIVCON_PATH}/src/solvers/multi_backend/portfolio-solver -o $sfile.out.json --verbose $sfile.ext.json
 
     ## Sometimes chuffed does not sto running
     pkill fzn-chuffed
